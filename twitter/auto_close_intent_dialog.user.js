c86d55b98db81e6d33f3d33b667d69f135a3bdc4
KID the Euforia <kid0725+github@gmail.com>
2012-11-19 12:32:23 +0900

    Twitter Web Intentのダイアログを自動的に閉じるヤツ

---
+++ twitter/auto_close_intent_dialog.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        [twitter] auto close intent dialog
+// @revision    1
+// @author      KID the Euforia a.k.a. blueberrystream
+// @description web intentで開いたダイアログを自動的に閉じるようにします。
+// @namespace   http://kid0725.usamimi.info
+// @include     https://twitter.com/intent/tweet/complete?*
+// ==/UserScript==
+
+void(function() {
+
+	window.close();
+
+})();

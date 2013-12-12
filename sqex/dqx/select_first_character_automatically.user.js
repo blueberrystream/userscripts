96fe87eda7e53e46b64e73d43cdfcccdb1dcbc29
KID the Euforia <kid0725@gmail.com>
2013-12-12 13:11:57 +0900

    DQX 目覚めし冒険者の広場でプレゼントのじゅもんやアイテムコードを入力するときに、自動的に最初のキャラを選択するようにするヤツ
    Signed-off-by: KID the Euforia <kid0725@gmail.com>

---
+++ sqex/dqx/select_first_character_automatically.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name        [sqex][dqx] select first character automatically
+// @revision    1
+// @author      KID the Euforia a.k.a. blueberrystream
+// @description DQX 目覚めし冒険者の広場でプレゼントのじゅもんやアイテムコードを入力するときに、自動的に最初のキャラを選択するようにするヤツ
+// @namespace   http://kid0725.usamimi.info
+// @include     http://hiroba.dqx.jp/sc/campaignCode/itemcode/
+// @include     http://hiroba.dqx.jp/sc/campaignCode/regcode/
+// ==/UserScript==
+
+// Chrome の userscript （とConsole）で jQuery を使う方法 - 弘法にも筆の誤り http://iwa4.hatenablog.com/entry/2013/07/02/181645
+(function (callback) {
+	var script = document.createElement("script");
+	script.setAttribute("src", "//code.jquery.com/jquery-2.0.3.min.js");
+	script.addEventListener('load', function() {
+		var script = document.createElement("script");
+		script.textContent = "(" + callback.toString() + ")(jQuery.noConflict(true));";
+		document.body.appendChild(script);
+	}, false);
+	document.body.appendChild(script);
+})(function ($) {
+	var firstRadio = $(':radio:first'), code = $('input[name=code]'), code1 = $('input[name=code1]');
+	if (firstRadio) {
+		firstRadio.click();
+
+		if (code) {
+			code.focus();
+		}
+		if (code1) {
+			code1.focus();
+		}
+	}
+});

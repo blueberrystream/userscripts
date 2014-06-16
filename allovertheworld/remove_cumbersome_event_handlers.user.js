eac9cd921576a790a9b8dd5df5f0009aebcae108
KID the Euforia <kid0725+github@gmail.com>
2014-06-16 19:30:57 +0900

    Tampermonkey対応

--- allovertheworld/remove_cumbersome_event_handlers.user.js
+++ allovertheworld/remove_cumbersome_event_handlers.user.js
@@ -1,23 +1,17 @@
 // ==UserScript==
 // @name        [all] remove cumbersome event handlers
-// @revision    1
-// @author      KID the Euforia a.k.a. blueberrystream
-// @description 邪魔なイベントハンドラーを削除します
 // @namespace   http://kid0725.usamimi.info
-// @include     http://*
-// @include     https://*
+// @version     1.0
+// @author      KID the Euforia a.k.a. blueberrystream
+// @description remove event handlers of oncut, oncopy, onpaste and oncontextmenu
+// @homepage    https://github.com/blueberrystream/userscripts/
+// @match       *://*/*
+// @grant       none
+// @require     http://code.jquery.com/jquery-2.1.1.min.js
+// @copyright   2014+, KID the Euforia a.k.a. blueberrystream
+// @license     MIT License
 // ==/UserScript==
 
-// Chrome の userscript （とConsole）で jQuery を使う方法 - 弘法にも筆の誤り http://iwa4.hatenablog.com/entry/2013/07/02/181645
-(function (callback) {
-	var script = document.createElement("script");
-	script.setAttribute("src", "//code.jquery.com/jquery-2.1.1.min.js");
-	script.addEventListener('load', function() {
-		var script = document.createElement("script");
-		script.textContent = "(" + callback.toString() + ")(jQuery.noConflict(true));";
-		document.body.appendChild(script);
-	}, false);
-	document.body.appendChild(script);
-})(function ($) {
+(function() {
 	$('*').off('oncut oncopy onpaste oncontextmenu').removeAttr('oncut oncopy onpaste oncontextmenu');
-});
+})();

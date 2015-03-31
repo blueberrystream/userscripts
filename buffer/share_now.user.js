66a1da77f1c82f3e6417cf1c537aa01b3d21e9f8
KID the Euforia <blueberrystream@users.noreply.github.com>
2015-03-31 12:34:36 +0900

    buffer.comに対応

--- buffer/share_now.user.js
+++ buffer/share_now.user.js
@@ -1,24 +1,19 @@
 // ==UserScript==
 // @name        [bufferapp.com] share now!!
 // @namespace   http://kid0725.usamimi.info
-// @version     1.0
+// @version     1.1
 // @author      KID the Euforia a.k.a. blueberrystream
 // @description 頼むからenqueueを前面に押し出すのやめてくれ
 // @homepage    https://github.com/blueberrystream/userscripts/
 // @match       https://bufferapp.com/add/?url=*
+// @match       https://buffer.com/add/?url=*
 // @grant       none
-// @require     http://code.jquery.com/jquery-2.1.1.min.js
+// @require     http://code.jquery.com/jquery-2.1.3.min.js
 // @copyright   2014+, KID the Euforia a.k.a. blueberrystream
 // @license     MIT License
 // ==/UserScript==
 
 void(function() {
-	var shareNow = $('#share-now').clone(true)
-		.attr('tabindex', 2).prop('tabindex', 2)
-		.addClass('button')
-		.on('keypress', function(){ this.click(); });
-	$('#share-later')
-		.removeAttr('type').removeProp('type')
-		.removeAttr('tabindex').removeProp('tabindex')
-		.before(shareNow);
+	var $shareNow = $('#share-now').clone(true).addClass('button').removeClass('dropdown-option');
+	$('.overlay-actions-right').prepend($shareNow);
 })();

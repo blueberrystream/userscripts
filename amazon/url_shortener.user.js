fde9ca6c1fab2f3262edaf9db54d48552922e5ff
Fumihito Hachinohe <blueberrystream@users.noreply.github.com>
2017-05-18 14:18:47 +0900

    Update url_shortener.user.js

--- amazon/url_shortener.user.js
+++ amazon/url_shortener.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name        [amazon.co.jp] URL Shortener
 // @namespace   http://kid0725.usamimi.info
-// @version     1.2
+// @version     1.3
 // @author      KID the Euforia a.k.a. blueberrystream
 // @description exclude unnecessary parameters from amazon.co.jp urls.
 // @homepage    https://github.com/blueberrystream/userscripts/
-// @match       *://www.amazon.co.jp/*
+// @match       https://www.amazon.co.jp/*
 // @grant       none
 // @copyright   2016+, KID the Euforia a.k.a. blueberrystream
 // @license     MIT License
 // ==/UserScript==
 
 (function() {
-	if (location.href.match(/https:\/\/www\.amazon\.co\.jp\/dp\/[a-zA-Z0-9]{10}/)) {
-		return;
-	}
+    if (location.href.match(/^https:\/\/www\.amazon\.co\.jp\/dp\/[a-zA-Z0-9]{10}$/)) {
+        return;
+    }
 
     var asin = document.getElementById('ASIN');
     if (asin !== '' && asin !== null && asin !== undefined) {
